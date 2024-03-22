ad12e008fd6066c987735ae076c42760f673f814
eddie <v_haiyang.wang@pactera.com>
2024-03-22 20:08:22 +0900

    feat: chapter 6

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/cool-page',
+                destination: '/dashboard'
+            }
+        ];
+    }
+};
 
 module.exports = nextConfig;
